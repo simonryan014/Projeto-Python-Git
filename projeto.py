ff247db6d61c2330047d850c1da486909edb0657
simonryan014 <simonryan132@gmail.com>
2024-04-05 04:13:31 -0800

    Gerador de senha em Python

---
+++ projeto.py
@@ -0,0 +1,11 @@
+import random 
+import string
+
+def gerar_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for i in range(comprimento))
+    return senha
+
+comprimento_senha = int(input("Digite o comprimento da senha desejada:"))
+nova_senha = gerar_senha(comprimento_senha)
+print("Sua nova senha é:", nova_senha)
